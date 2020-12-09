45f63f9b72b81fe7da138bdb43eb3999774beb63
Nagy Zoltan <zolya1999@gmail.com>
2020-12-09 22:27:25 +0100

    Added composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '4ada07727b238ebead28cca973de853bd2dd0ef2',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4ada07727b238ebead28cca973de853bd2dd0ef2',
+    ),
+  ),
+);

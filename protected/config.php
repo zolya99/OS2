945214d20235ea47ab57d3aa6cc363fcb2b4bdfc
Nagy Zoltan <zolya1999@gmail.com>
2020-12-12 22:38:29 +0100

    Modified

--- protected/config.php
+++ protected/config.php
@@ -7,9 +7,9 @@ define('DATABASE_CONTROLLER', PROTECTED_DIR.'database.php');
 define('USER_MANAGER', PROTECTED_DIR. 'userManager.php');
 
 define('DB_TYPE', 'mysql');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'miniproject');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', 'sql7.freemysqlhosting.net');
+define('DB_NAME', 'sql7381752');
+define('DB_USER', 'sql7381752');
+define('DB_PASS', '9ppq6aDyfn');
 define('DB_CHARSET', 'utf8');
 ?>
